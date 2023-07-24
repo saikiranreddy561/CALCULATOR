81cef9c546f3e1864a30a3dffc287d2ab8f9465b
saikiranreddy561 <102889718+saikiranreddy561@users.noreply.github.com>
2023-07-24 19:44:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let input=document.getElementById('input');
+let buttons=document.querySelectorAll('button');
+let string="";
+let arr=Array.from(buttons);
+arr.forEach((button) => {
+    button.addEventListener('click',(e)=>{
+    if(e.target.innerHTML == '='){
+        string=eval(string);
+        document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML=="AC"){
+        string="";
+        document.querySelector('input').value=string;
+
+    }
+    else if(e.target.innerHTML=="DEL"){
+        string=string.substring(0,string.length-1);
+        document.querySelector('input').value=string;
+    }
+    else{
+        string+=e.target.innerHTML;
+        document.querySelector('input').value=string; 
+    }
+
+}) 
+
+})
